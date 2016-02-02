bd7f92ba605db5415ec74ce39177688f10ec4ced
Thendup Tsering <thendup11@gmail.com>
2016-02-02 18:24:45 -0500

    Traffic Synchronization Implementation 1

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -4,46 +4,83 @@
 #include <synch.h>
 #include <opt-A1.h>
 
-/* 
+/*
  * This simple default synchronization mechanism allows only vehicle at a time
  * into the intersection.   The intersectionSem is used as a a lock.
  * We use a semaphore rather than a lock so that this code will work even
  * before locks are implemented.
  */
 
-/* 
+/*
  * Replace this default synchronization mechanism with your own (better) mechanism
  * needed for your solution.   Your mechanism may use any of the available synchronzation
- * primitives, e.g., semaphores, locks, condition variables.   You are also free to 
+ * primitives, e.g., semaphores, locks, condition variables.   You are also free to
  * declare other global variables if your solution requires them.
  */
 
 /*
  * replace this with declarations of any synchronization and other variables you need here
  */
-static struct semaphore *intersectionSem;
+static struct lock *intersectionLock;
+static struct cv *cvDestinationNorth;
+static struct cv *cvDestinationSouth;
+static struct cv *cvDestinationEast;
+static struct cv *cvDestinationWest;
+// Boolean of blocked paths:
+volatile static bool N2E, N2S, N2W;
+volatile static bool E2S, E2W, E2N;
+volatile static bool S2W, S2N, S2E;
+volatile static bool W2N, W2E, W2S;
 
 
-/* 
+
+/*
  * The simulation driver will call this function once before starting
  * the simulation
  *
  * You can use it to initialize synchronization and other variables.
- * 
+ *
  */
 void
 intersection_sync_init(void)
 {
-  /* replace this default implementation with your own implementation */
-
-  intersectionSem = sem_create("intersectionSem",1);
-  if (intersectionSem == NULL) {
-    panic("could not create intersection semaphore");
+  intersectionLock = lock_create("intersectionLock");
+  if (intersectionLock == NULL) {
+    panic("could not create intersection lock");
   }
+  cvDestinationNorth = cv_create('cvDestinationNorth');
+  if (cvDestinationNorth == NULL) {
+    panic("could not create condition variable cvDestinationNorth");
+  }
+  cvDestinationSouth = cv_create('cvDestinationSouth');
+  if (cvDestinationSouth == NULL) {
+    panic("could not create condition variable cvDestinationSouth");
+  }
+  cvDestinationEast = cv_create('cvDestinationEast');
+  if (cvDestinationEast == NULL) {
+    panic("could not create condition variable cvDestinationEast");
+  }
+  cvDestinationWest = cv_create('cvDestinationWest');
+  if (cvDestinationWest == NULL) {
+    panic("could not create condition variable cvDestinationWest");
+  }
+  // All paths are free before traffic starts
+  N2E = false;
+  N2S = false;
+  N2W = false;
+  E2S = false;
+  E2W = false;
+  E2N = false;
+  S2W = false;
+  S2N = false;
+  S2E = false;
+  W2N = false;
+  W2E = false;
+  W2S = false;
   return;
 }
 
-/* 
+/*
  * The simulation driver will call this function once after
  * the simulation has finished
  *
@@ -53,16 +90,59 @@ intersection_sync_init(void)
 void
 intersection_sync_cleanup(void)
 {
-  /* replace this default implementation with your own implementation */
-  KASSERT(intersectionSem != NULL);
-  sem_destroy(intersectionSem);
+  KASSERT(intersectionLock != NULL);
+  lock_destroy(intersectionLock);
+  KASSERT(cvDestinationNorth != NULL);
+  cv_destroy(cvDestinationNorth);
+  KASSERT(cvDestinationSouth != NULL);
+  cv_destroy(cvDestinationSouth);
+  KASSERT(cvDestinationEast != NULL);
+  cv_destroy(cvDestinationEast);
+  KASSERT(cvDestinationWest != NULL);
+  cv_destroy(cvDestinationWest);
+}
+
+/*
+  Intersection will be split into 4 quadrants:
+  * * * * * *
+  * NW * NE *
+  * * * * * *
+  * SW * SE *
+  * * * * * *
+*/
+bool
+blockedNW(void)
+{
+  ret = (N2W && N2S && N2E && E2W && E2S && S2W);
+  return ret;
+}
+
+bool
+blockedNE(void)
+{
+  ret = (E2N && E2W && E2S && S2N && S2W && W2N);
+  return ret;
+}
+
+bool
+blockedSE(void)
+{
+  ret = (S2E && S2N && S2W && W2E && W2N && N2E);
+  return ret;
+}
+
+bool
+blockedSW(void)
+{
+  ret = (W2S && W2E && W2N && N2S && N2E && E2S);
+  return ret;
 }
 
 
 /*
  * The simulation driver will call this function each time a vehicle
  * tries to enter the intersection, before it enters.
- * This function should cause the calling simulation thread 
+ * This function should cause the calling simulation thread
  * to block until it is OK for the vehicle to enter the intersection.
  *
  * parameters:
@@ -73,13 +153,137 @@ intersection_sync_cleanup(void)
  */
 
 void
-intersection_before_entry(Direction origin, Direction destination) 
+intersection_before_entry(Direction origin, Direction destination)
 {
-  /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  P(intersectionSem);
+  KASSERT(intersectionLock != NULL);
+  lock_acquire(intersectionLock);
+  switch (origin) {
+    case north:
+      switch (destination) {
+        case south:
+          if (!N2S) {
+            KASSERT(cvDestinationSouth != NULL);
+            while (blockedNW() && blockedSW()) {
+              cv_wait(cvDestinationSouth,intersectionLock);
+            }
+            N2S = true;
+          }
+          break;
+        case east:
+          if (!N2E) {
+            KASSERT(cvDestinationEast != NULL);
+            while (blockedNW() && blockedSW() && blockedSE()) {
+              cv_wait(cvDestinationEast,intersectionLock);
+            }
+            N2E = true;
+          }
+          break;
+        case west:
+          if (!N2W) {
+            KASSERT(cvDestinationWest != NULL);
+            while (blockedNW()) {
+              cv_wait(cvDestinationWest,intersectionLock);
+            }
+            N2W = true;
+          }
+          break;
+      }
+      break;
+    case east:
+      switch (destination) {
+        case west:
+          if (!E2W) {
+            KASSERT(cvDestinationWest != NULL);
+            while (blockedNE() && blockedNW()) {
+              cv_wait(cvDestinationWest,intersectionLock);
+            }
+            E2W = true;
+          }
+          break;
+        case south:
+          if (!E2S) {
+            KASSERT(cvDestinationSouth != NULL);
+            while (blockedNE() && blockedNW() && blockedSW()) {
+              cv_wait(cvDestinationWest,intersectionLock);
+            }
+            E2S = true;
+          }
+          break;
+        case north:
+          if (!E2N) {
+            KASSERT(cvDestinationNorth != NULL);
+            while (blockedNE()) {
+              cv_wait(cvDestinationNorth,intersectionLock);
+            }
+            E2N = true;
+          }
+          break;
+      }
+      break;
+    case south:
+      switch (destination) {
+        case north:
+          if (!S2N) {
+            KASSERT(cvDestinationNorth != NULL);
+            while (blockedSE() && blockedNE()) {
+              cv_wait(cvDestinationNorth,intersectionLock);
+            }
+            S2N = true;
+          }
+          break;
+        case east:
+          if (!S2E) {
+            KASSERT(cvDestinationEast != NULL);
+            while (blockedSE()) {
+              cv_wait(cvDestinationEast,intersectionLock);
+            }
+            S2E = true;
+          }
+          break;
+        case west:
+          if (!S2W) {
+            KASSERT(cvDestinationWest != NULL);
+            while (blockedSE() && blockedNE() && blockedNW()) {
+              cv_wait(cvDestinationWest,intersectionLock);
+            }
+            S2W = true;
+          }
+          break;
+      }
+      break;
+    case west:
+      switch (destination) {
+        case east:
+          if (!W2E) {
+            KASSERT(cvDestinationEast != NULL);
+            while (blockedSW() && blockedSE()) {
+              cv_wait(cvDestinationEast,intersectionLock);
+            }
+            W2E = true;
+          }
+          break;
+        case south:
+          if (!W2S) {
+            KASSERT(cvDestinationSouth != NULL);
+            while (blockedSW()) {
+              cv_wait(cvDestinationSouth,intersectionLock);
+            }
+            W2S = true;
+          }
+          break;
+        case north:
+          if (!W2N) {
+            KASSERT(cvDestinationNorth != NULL);
+            while (blockedSW() && blockedSE() && blockedNE()) {
+              cv_wait(cvDestinationNorth,intersectionLock);
+            }
+            W2N = true;
+          }
+          break;
+        }
+      break;
+  }
+  lock_release(intersectionLock);
 }
 
 
@@ -95,11 +299,72 @@ intersection_before_entry(Direction origin, Direction destination)
  */
 
 void
-intersection_after_exit(Direction origin, Direction destination) 
+intersection_after_exit(Direction origin, Direction destination)
 {
-  /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  V(intersectionSem);
+  switch (origin) {
+    case north:
+      switch (destination) {
+        case south:
+          N2S = false;
+          cv_signal(cvDestinationSouth,intersectionLock);
+          break;
+        case east:
+          N2E = false;
+          cv_signal(cvDestinationEast,intersectionLock);
+          break;
+        case west:
+          N2W = false;
+          cv_signal(cvDestinationWest,intersectionLock);
+          break;
+      }
+      break;
+    case east:
+      switch (destination) {
+        case west:
+          E2W = false;
+          cv_signal(cvDestinationWest,intersectionLock);
+          break;
+        case south:
+          E2S = false;
+          cv_signal(cvDestinationSouth,intersectionLock);
+          break;
+        case north:
+          E2N = false;
+          cv_signal(cvDestinationNorth,intersectionLock);
+          break;
+      }
+      break;
+    case south:
+      switch (destination) {
+        case north:
+          S2N = false;
+          cv_signal(cvDestinationNorth,intersectionLock);
+          break;
+        case east:
+          S2E = false;
+          cv_signal(cvDestinationEast,intersectionLock);
+          break;
+        case west:
+          S2W = false;
+          cv_signal(cvDestinationWest,intersectionLock);
+          break;
+      }
+      break;
+    case west:
+      switch (destination) {
+        case east:
+          W2E = false;
+          cv_signal(cvDestinationEast,intersectionLock);
+          break;
+        case south:
+          W2S = false;
+          cv_signal(cvDestinationSouth,intersectionLock);
+          break;
+        case north:
+          W2N = false;
+          cv_signal(cvDestinationNorth,intersectionLock);
+          break;
+        }
+      break;
+  }
 }
--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -287,13 +287,13 @@ cv_wait(struct cv *cv, struct lock *lock)
 void
 cv_signal(struct cv *cv, struct lock *lock)
 {
-  (void)lock
+  (void)lock;
   wchan_wakeone(cv->cv_wchan);
 }
 
 void
 cv_broadcast(struct cv *cv, struct lock *lock)
 {
-  (void)lock
+  (void)lock;
   wchan_wakeall(cv->cv_wchan);
 }
